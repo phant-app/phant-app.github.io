9a77cea19d3fb569fac34e8ed69427f7c19527db
Ronald Pereira <ronaldpereira01@gmail.com>
2026-04-21 13:47:03 -0400

    add umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import tailwindcss from '@tailwindcss/vite';
+import umami from "@yeskunall/astro-umami";
+
 
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.SITE_URL ?? 'http://localhost:4321',
 	integrations: [
+		umami({ id: '25161f87-59c3-4c4b-a4f9-894a18cd5afd'}),
 		starlight({
 			title: 'Phant Docs',
 			customCss: ['./src/styles/tailwind.css', './src/styles/docs-theme.css'],
